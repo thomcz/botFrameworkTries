64223841b7d15e76bd04dec629ee29fc88ae2935
thomcz <thomas.czogalik@gmx.de>
2017-05-29 20:48:24 +0200

    changed to simple bot

--- app.js
+++ app.js
@@ -7,28 +7,14 @@ var botConnectorOptions = {
     appPassword: 'pNny7sFzX2cVYYsar1fs9gm'
 };
 
-// Create bot
-var connector = new builder.ChatConnector(botConnectorOptions);
-var bot = new builder.UniversalBot(connector);
-
+var bot = new builder.UniversalBot(connector);  
 bot.dialog('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
+    session.send('Hello World');
 });
 
 // Setup Restify Server
 var server = restify.createServer();
-
-// Handle Bot Framework messages
 server.post('/api/messages', connector.listen());
-
-// Serve a static web page
-server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
-
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
