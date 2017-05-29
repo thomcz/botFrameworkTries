838f5de2eb485cf837d747557cda4b6fa39e2a9c
thomcz <thomas.czogalik@gmx.de>
2017-05-29 20:53:57 +0200

    changed to echo bot

--- app.js
+++ app.js
@@ -7,14 +7,28 @@ var botConnectorOptions = {
     appPassword: 'pNny7sFzX2cVYYsar1fs9gm'
 };
 
-var bot = new builder.UniversalBot(connector);  
+// Create bot
+var connector = new builder.ChatConnector(botConnectorOptions);
+var bot = new builder.UniversalBot(connector);
+
 bot.dialog('/', function (session) {
-    session.send('Hello World');
+    
+    //respond with user's message
+    session.send("You said " + session.message.text);
 });
 
 // Setup Restify Server
 var server = restify.createServer();
+
+// Handle Bot Framework messages
 server.post('/api/messages', connector.listen());
+
+// Serve a static web page
+server.get(/.*/, restify.serveStatic({
+	'directory': '.',
+	'default': 'index.html'
+}));
+
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
